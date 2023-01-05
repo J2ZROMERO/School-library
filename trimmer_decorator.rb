af1dccbe977b7f0e9bc071c48138ccb997e9e1c6
Jhosep <jose-zromero@hotmail.com>
2023-01-05 07:53:59 -0600

    Apply linters

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -9,7 +9,6 @@ class TrimmerDecorator < Person
   def correct_name()
     return @nameable.correct_name.slice(0, 10) if @nameable.correct_name.length > 10
 
-
     @nameable.correct_name
   end
 end
